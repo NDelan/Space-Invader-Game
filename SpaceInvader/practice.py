f03fff29319ae9be081b6db8adb7e873751cf541
NDelan <nutakordelanyo2@gmail.com>
2023-01-03 21:18:40 -0500

    Implement Mechanical movements

--- SpaceInvader/practice.py
+++ SpaceInvader/practice.py
@@ -11,10 +11,37 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('Images\spaceship.png')
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load('Images\cade.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+playerX_change = 0
+
+def player(x, y):
+    screen.blit(playerImg, (playerX, playerY))
+
 # Game Loop
 # Close game window when close button is clicked
 running = True
 while running:
+
+    # Set Screen Window background colour
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        # Key Stroke events
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -1
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+    player(playerX, playerY)
+    pygame.display.update()
