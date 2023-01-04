7cfca324f6d79711b365c28a071bb333150b1ac5
NDelan <nutakordelanyo2@gmail.com>
2023-01-03 21:45:07 -0500

    Create enemy at random starting point

--- SpaceInvader/practice.py
+++ SpaceInvader/practice.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Initialize the pygame
 pygame.init()
@@ -16,10 +17,20 @@ playerImg = pygame.image.load('Images\cade.png')
 playerX = 370
 playerY = 480
 playerX_change = 0
-playerX_change = 0
+playerY_change = 0
+
+# Enemy
+enemyImg = pygame.image.load('Images\lien.png')
+enemyX = random.randint(0,800)
+enemyY = random.randint(20,60)
+enemyX_change = 0
+enemyY_change = 0
 
 def player(x, y):
     screen.blit(playerImg, (playerX, playerY))
+
+def enemy(x, y):
+    screen.blit(enemyImg, (enemyX, enemyY))
 
 # Game Loop
 # Close game window when close button is clicked
@@ -42,6 +53,12 @@ while running:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
+
     playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
     player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
